7a6f368cd3d59cae0bdb54563f4f03ea8e8d0840
excelkks <excelkks@hotmail.com>
2023-02-28 23:11:45 +0800

    Update: change layout

--- crypto.js
+++ crypto.js
@@ -4,6 +4,98 @@ const utf8Dencoder = new TextDecoder();
 const iterations = 1000;
 const salt = utf8Encoder.encode('XHWnDAT6ehMVY2zD');
 
+
+/** 获取加密按钮 */
+let encryptBtn = document.getElementById('encryptBtn');
+/** 获取解密按钮 */
+let decryptBtn = document.getElementById('decryptBtn');
+/** 获取复制原文按钮 */
+let copyDecryptedBtn = document.getElementById('copyDecrypted');
+/** 获取复制密文按钮 */
+let copyEncryptedBtn = document.getElementById('copyEncrypted');
+/** 获取复制清空按钮 */
+let clearAllBtn = document.getElementById('clearAll');
+
+/** 原文内容 */
+let decryptedData = document.getElementById('decryptedData');
+/** 密匙内容 */
+let pass = document.getElementById('pass');
+/** 加密内容 */
+let encryptedData = document.getElementById('encryptedData');
+
+/** 加密绑定点击事件 */
+encryptBtn.addEventListener('click', handleEncryptClick)
+/** 解密绑定点击事件 */
+decryptBtn.addEventListener('click', handleDecryptClick);
+/** 复制原文按钮绑定点击事件 */
+copyDecryptedBtn.addEventListener('click', copyDecryptedFun);
+/** 复制密文按钮绑定点击事件 */
+copyEncryptedBtn.addEventListener('click', copyEncryptedFun);
+/** 清空按钮绑定点击事件 */
+clearAllBtn.addEventListener('click', clearAllFun);
+
+
+/** 拷贝原文操作函数 */
+async function copyDecryptedFun() {
+    await navigator.clipboard.writeText(decryptedData.value);
+}
+/** 拷贝密文操作函数 */
+async function copyEncryptedFun() {
+    await navigator.clipboard.writeText(encryptedData.value);
+}
+
+/** 清空数据函数*/
+async function clearAllFun() {
+    decryptedData.value='';
+    pass.value='';
+    encryptedData.value='';
+}
+
+// 加密
+async function handleEncryptClick() {
+    const text = decryptedData.value;
+    const password = pass.value;
+    const base64Encoded = await encryptToBase64(text, password);
+    encryptedData.value = base64Encoded;
+}
+
+// 解密
+async function handleDecryptClick() {
+    const base64Encoded = encryptedData.value;
+    const password = pass.value;
+    const text = await decryptFromBase64(base64Encoded, password);
+    decryptedData.value = text;
+}
+
+/** -----------------------以下内容为加密算法------------------------------------- */
+
+async function decryptFromBase64(base64Encoded, password) {
+    const bytesToDecode = stringToArray(atob(base64Encoded));
+    return await decryptFromBytes(bytesToDecode, password);
+}
+
+function stringToArray(str) {
+    const result = [];
+    for(let i = 0; i < str.length; i++) {
+        result.push(str.charCodeAt(i));
+    }
+    return new Uint8Array(result);
+}
+
+async function decryptFromBytes(encryptedBytes, password) {
+    const vector = encryptedBytes.slice(0, vectorSize);
+    const encryptedTextBytes = encryptedBytes.slice(vectorSize);
+    const key = await deriveKey(password);
+    const decryptedBytes = await crypto.subtle.decrypt(
+        { name: 'AES-GCM', iv: vector},
+        key,
+        encryptedTextBytes
+    );
+
+    const decryptedText = utf8Dencoder.decode(decryptedBytes);
+    return decryptedText;
+}
+
 async function deriveKey(password) {
     const buffer = utf8Encoder.encode(password)
     const key = await crypto.subtle.importKey(
@@ -30,7 +122,6 @@ async function deriveKey(password) {
     );
     return privateKey;
 }
-
 
 async function encryptToBytes(text, password) {
     const key = await deriveKey(password);
@@ -69,49 +160,8 @@ async function encryptToBase64(text, password) {
     return base64Text;
 }
 
-function stringToArray(str) {
-    const result = [];
-    for(let i = 0; i < str.length; i++) {
-        result.push(str.charCodeAt(i));
-    }
-    return new Uint8Array(result);
-}
 
-async function decryptFromBytes(encryptedBytes, password) {
-    const vector = encryptedBytes.slice(0, vectorSize);
-    const encryptedTextBytes = encryptedBytes.slice(vectorSize);
-    const key = await deriveKey(password);
-    const decryptedBytes = await crypto.subtle.decrypt(
-        { name: 'AES-GCM', iv: vector},
-        key,
-        encryptedTextBytes
-    );
 
-    const decryptedText = utf8Dencoder.decode(decryptedBytes);
-    return decryptedText;
-}
 
-async function decryptFromBase64(base64Encoded, password) {
-    const bytesToDecode = stringToArray(atob(base64Encoded));
-    return await decryptFromBytes(bytesToDecode, password);
-}
 
-async function handleDecryptClick() {
-    let decryptedData =  document.getElementById('decryptedData');
-    let encryptedData =  document.getElementById('encryptedData');
-    let pass =  document.getElementById('pass');
-    const base64Encoded = encryptedData.value;
-    const password = pass.value;
-    const text = await decryptFromBase64(base64Encoded, password);
-    decryptedData.value = text;
-}
 
-async function handleEncryptClick() {
-    let decryptedData =  document.getElementById('decryptedData');
-    let encryptedData =  document.getElementById('encryptedData');
-    let pass =  document.getElementById('pass');
-    const text = decryptedData.value;
-    const password = pass.value;
-    const base64Encoded = await encryptToBase64(text, password);
-    encryptedData.value = base64Encoded;
-}
